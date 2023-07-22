278ce2ecdc6c371cdd6891b61c4615825af7ec2d
shoot1234 <phenix.endli9@gmail.com>
2023-07-22 20:13:09 +0900

    投稿文章の編集機能の追加

--- src/main/java/com/example/demo/DiaryController.java
+++ src/main/java/com/example/demo/DiaryController.java
@@ -40,10 +40,32 @@ public class DiaryController {
 	public String add(Model model, @Valid NewDiaryForm newDiaryForm, BindingResult bindingResult) {
 	    //バリデーションエラーがあるかどうかチェックして、エラーがあるなら新規登録処理をせずに、一覧表示メソッドを呼び出す
 	    if (bindingResult.hasErrors()) {
+	    	//メソッドを呼び出して戻り値をretuneでViewへ。newDiaryFormのStringフィールドにはPostリクエストされた値ではなくエラーメッセージが代入されている
 	    	return summary(model, newDiaryForm);
 	    }
 	    //ChronoUnit.SECONDSで秒以下を切り捨て
 	    Diary diary = new Diary(newDiaryForm.getNewdiary(), LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS));
+	    diaryRepository.save(diary);
+	    return "redirect:/diary/summary";
+	  }
+	
+	//編集画面を表示する
+	  @GetMapping("edit")
+	  String edit(Model model, EditDiaryForm editDiaryForm) {
+		//findByIdメソッドの戻り値はOptional<T>
+	    Diary diary = diaryRepository.findById(editDiaryForm.getId()).get();
+	    model.addAttribute("editdiary", diary);
+	    return "edit";
+	  }
+
+	  //投稿文章を更新する
+	  @PostMapping("update")
+	  String update(Model model, @Valid EditDiaryForm editDiaryForm, BindingResult bindingResult) {
+	    if (bindingResult.hasErrors()) {
+	      return edit(model, editDiaryForm);
+	    }
+	    Diary diary = new Diary(editDiaryForm.getId(), editDiaryForm.getUpdateddiary(), LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS));
+	    //実装クラスのSimpleJpaRepositoryのsaveメソッドは、DBのテーブルにレコードが存在するかどうかで実行するSQLをinsertかupdateで使い分ける。48行目と同じメソッドでも内部のSQLは別。
 	    diaryRepository.save(diary);
 	    return "redirect:/diary/summary";
 	  }
